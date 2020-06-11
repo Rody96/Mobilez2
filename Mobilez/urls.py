5eb8ea42e0fcda0da144ea33f2c84ba99a429ec9
fshatskiy <f.shatskiy@students.ephec.be>
2020-06-11 19:37:38 +0200

    bouton démarrer résolu

--- Mobilez/urls.py
+++ Mobilez/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from allauth.account.views import LoginView, ConfirmEmailView
 from allauth.account import views as allauth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LoginView.as_view(), name="login"),
-    path('confirm-email/', ConfirmEmailView.as_view(), name="account_confirm_email"),
+    # re_path(r"^confirm-email/(?P<key>[-:\w]+)/$", ConfirmEmailView.as_view(), name="account_confirm_email"),
     path('', include('allauth.urls')),
     path('', allauth_views.login, name="account_login"),
     path('accueil/', include('accueil.urls')),
